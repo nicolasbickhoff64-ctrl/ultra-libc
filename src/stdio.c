04fe9376ffc99f766be087b7e801bd70c39051b9
nicolasbickhoff64 <nicolasbickhoff64@gmail.com>
2026-03-08 07:31:08 -0300

    Update stdio.c

--- src/stdio.c
+++ src/stdio.c
@@ -6,6 +6,49 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
+static void intToStr(int num, char *str) {
+    int i = 0;
+    int isNegative = 0;
+
+    // Tratar 0 explicitamente
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return;
+    }
+
+    // Tratar números negativos
+    if (num < 0) {
+        isNegative = 1;
+        num = -num;
+    }
+
+    // Processar dígitos individuais
+    while (num != 0) {
+        int rem = num % 10;
+        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+        num = num / 10;
+    }
+
+    // Adicionar sinal negativo se necessário
+    if (isNegative) {
+        str[i++] = '-';
+    }
+
+    str[i] = '\0'; // Terminador nulo
+
+    // Inverter a string
+    int start = 0;
+    int end = i - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
 extern long wrap(long ret);
 
 int printf(char *s, ...) {
@@ -28,6 +71,16 @@ int printf(char *s, ...) {
             } else if (*p == 'c') {
                 char c = (char)va_arg(args, int);
                 int ret = write(1, &c, 1);
+                if (ret < 0) {
+                    va_end(args);
+                    return -1;
+                }
+                total += ret;
+            } else if (*p == 'd') {
+                int n = (int)va_arg(args, int);
+                char buffer[200];
+                intToStr(n, buffer);
+                int ret = write(1, buffer, strlen(buffer));
                 if (ret < 0) {
                     va_end(args);
                     return -1;
